2904d227ac1caf39306517d598d58d469dd6db03
Ash <aswinreddy619@gmail.com>
2023-09-05 20:24:30 +0530

    Adding Greeting File

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GreetingProgramInPython
 Program to greet the user Based on Inputs
+
+Project Details: 
+    Write a Python program that:
+    Takes user input for their name and age.
+    Greets the user with a personalized message.
+    Calculates and prints the year they'll turn 100 years old.
---
+++ greet.py
@@ -0,0 +1,50 @@
+import datetime
+
+def checkValidInteger(input_str):
+    try:
+        int(input_str)
+        return True
+    except ValueError:
+        return False
+    
+user_name = input("Plz enter your Name:\n")
+user_age  = input("Plz enter your Age:\n")
+
+while not checkValidInteger(user_age):
+   user_age = input("The number you entered is invalid, Plz enter your right Age:\n")
+
+
+greeting = f"Hello, {user_name}! Nice to meet you."
+current_year = datetime.datetime.now().year
+year_to_reach_100 = current_year + (100-int(user_age))
+year_100_msg = f"Did you know, you will turn 100 in year {year_to_reach_100}!."
+
+
+print(greeting, end="\n")
+if int(user_age) < 100:
+    print(year_100_msg, end="\n")
+
+
+
+# quotes
+
+# Children (0-19 years old):
+
+# Quote: "Believe in the magic within you, for you are capable of achieving wonderful things."
+# Fun Fact: "Did you know that honey never spoils? It's like a jar of magic that lasts forever!"
+# Young Adults (20-39 years old):
+
+# Quote: "Life is an adventure waiting to be embraced. Seize the opportunities and create your own path."
+# Fun Fact: "Octopuses have three hearts, just like you have a heart full of passion for your adventures."
+# Middle-Aged Adults (40-59 years old):
+
+# Quote: "The middle of your journey is not the end. It's a chance for renewal and rediscovery."
+# Fun Fact: "As you navigate life's twists and turns, remember that your taste for new experiences can evolve, just like your palate."
+# Older Adults (60-79 years old):
+
+# Quote: "The beauty of life is in its continuous evolution. Embrace change and savor the wisdom that comes with age."
+# Fun Fact: "Much like the Eiffel Tower, you too can stand tall and proud in any season of life, growing stronger with time."
+# Elderly (80+ years old):
+
+# Quote: "Your life is a masterpiece painted with years of experience and memories. Cherish every stroke."
+# Fun Fact: "Like the 'immortal jellyfish,' you've experienced the power of renewal, forever young in spirit."
